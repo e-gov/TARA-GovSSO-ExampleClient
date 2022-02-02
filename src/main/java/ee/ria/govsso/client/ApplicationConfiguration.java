0bbc9d3a6fff76ad36a210e88458521ed74b9ec4
Jorgen Heinsoo <jorgen.heinsoo@nortal.com>
2022-02-02 09:40:54 +0200

    GSSO-68 TLS support

--- src/main/java/ee/ria/govsso/client/ApplicationConfiguration.java
+++ src/main/java/ee/ria/govsso/client/ApplicationConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@DependsOn("SSLConfig")
 @Slf4j
 @Configuration
 @ConfigurationProperties(prefix = "govsso")
