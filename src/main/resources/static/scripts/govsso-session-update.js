8396eca68732ba273731eb64c8ec38e9d9d45085
Jorgen Heinsoo <jorgen.heinsoo@nortal.com>
2022-02-11 09:00:25 +0200

    GSSO-170 Console log session update result

--- src/main/resources/static/scripts/govsso-session-update.js
+++ src/main/resources/static/scripts/govsso-session-update.js
@@ -5,9 +5,15 @@ function updateGovssoSession() {
             // Makes sure cross-site requests (redirects to GOVSSO) are made with cookies, authorization headers
             // and TLS client certificates. GOVSSO needs only cookies.
             credentials: 'include'
+        }).then(function (response) {
+            if (response.ok) {
+                console.log('GOVSSO session successfully updated');
+            } else {
+                console.error('Error updating GOVSSO session');
+            }
         }).catch((error) => {
             // TODO: Handle error
-            console.error('Error updating govsso session:', error);
+            console.error('Error updating GOVSSO session:', error);
         });
     })();
 }
